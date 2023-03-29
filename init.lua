78c8a77eb2de567012e943236fcff31ac34235cf
zwyyy456 <zwyyy456@hotmail.com>
2023-03-29 17:05:06 +0800

    mac, vscode keyborard gc

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 if vim.g.vscode then
+  -- if true then
   -- Vscoe extension
   require("vscode.config.options")
+  require("vscode.config.keymaps")
+  -- require("vscode.init")
+  require("vscode.config.lazy")
+  -- require("vscode.plugins.disabled")
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
---
+++ lua/vscode/plugins/mini.lua
@@ -0,0 +1,67 @@
+  -- comments
+  return {
+  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>", desc = "Decrement selection", mode = "x" },
+    },
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = false},
+      indent = { enable = false, disable = { "python" } },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      ensure_installed = {
+        "bash",
+        "c",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = false,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+
+}
