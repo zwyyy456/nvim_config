ae1d5fd93eba433a46dfa7a7ee57d595135dca9c
zwyyy456 <zwyyy456@hotmail.com>
2023-07-30 22:50:20 +0800

    try Comment.nvim plugin, failed

--- lua/vscode/config/keymaps.lua
+++ lua/vscode/config/keymaps.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 -- local opts = { noremap = true, silent = true }
 -- vscode.neovim用于注释代码的按键映射
-keymap.set("v", "gc", "<Plug>VSCodeCommentary")
-keymap.set("n", "gc", "<cmd> lua require('vscode').call('editor.action.commentLine')<CR>")
-keymap.set("n", "gcc", "<cmd> lua require('vscode').call('editor.action.commentLine')<CR>")
+-- keymap.set("v", "gc", "<Plug>VSCodeCommentary")
+-- keymap.set("n", "gc", "<cmd> lua require('vscode').call('editor.action.commentLine')<CR>")
+-- keymap.set("n", "gcc", "<cmd> lua require('vscode').call('editor.action.commentLine')<CR>")
