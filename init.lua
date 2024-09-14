6ff2089c1f420e827df6ec8fc07c68ad20bc8bdb
zwyyy456 <zwyyy456@hotmail.com>
2024-09-14 09:59:32 +0800

    use lazy extras: vscode for vscode.neovim plugin

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
--- require("config.lazy")
 if vim.g.vscode then
   -- if true then
   -- Vscoe extension
-  require("vsc.config.options")
-  require("vsc.config.keymaps")
-  require("vsc.config.lazy")
+  require("config.lazy")
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
