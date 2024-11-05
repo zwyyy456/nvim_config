5e45c8c5aa34b1afa012436dfef4bfede25139fe
zwyyy456 <zwyyy456@hotmail.com>
2024-11-05 22:16:15 +0800

    fix(init):  is not needed any more

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
-if vim.g.vscode then
-  -- if true then
-  -- Vscoe extension
-  require("config.lazy")
-else
-  -- bootstrap lazy.nvim, LazyVim and your plugins
-  require("config.lazy")
-  -- require("config.osc52")
-end
+-- 由于 lazyvim 的 vscode extras 的存在，因此不再需要设置 if vim.g.vscode then ...
+require("config.lazy")
